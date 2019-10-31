0edc31ba786442b53a55deabf65b87ec68ac1dca
Asliddin Asliev <you@example.com>
2019-10-31 17:37:30 -0400

    start of the project

---
+++ ChallengeYourself/ChallengeYourslefViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChallengeYourself
+//
+//  Created by Asliddin Asliev on 10/31/19.
+//  Copyright © 2019 Asliddin Asliev. All rights reserved.
+//
+
+import UIKit
+
+class ChallengeYourselfViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
