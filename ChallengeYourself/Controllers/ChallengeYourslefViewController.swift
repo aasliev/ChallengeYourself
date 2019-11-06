41a16f94df164038869ace4183aec6b0e529f911
Asliddin Asliev <you@example.com>
2019-11-06 10:17:54 -0500

    Added a searchbar

---
+++ ChallengeYourself/Controllers/ChallengeYourslefViewController.swift
@@ -0,0 +1,132 @@
+//
+//  ViewController.swift
+//  ChallengeYourself
+//
+//  Created by Asliddin Asliev on 10/31/19.
+//  Copyright © 2019 Asliddin Asliev. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ChallengeYourselfViewController: UITableViewController {
+    var itemArray = [Challenge]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        loadItems()
+    }
+
+    
+    //MARK: -  TableView DataSource Methods
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChallengeCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row].nameOfTheChallenge
+        
+        return cell
+    }
+    
+    //MARK: -  TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+           tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        saveItems()
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    //MARK: - Add New Challenges
+    
+    @IBAction func addNewChallenge(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add New Challenge", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add Challenge", style: .default) { (action) in
+            //what will happen when the user clicks the add button
+            let newChallenge = Challenge(context: self.context)
+            newChallenge.nameOfTheChallenge = textField.text!
+            
+            self.loadItems()
+            self.saveItems()
+            
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new challenge"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    //MARK: - Model Manipulation Methods
+    
+    func saveItems()
+    {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context \(error)")
+        }
+        self.tableView.reloadData()
+
+    }
+    
+    func loadItems(with request: NSFetchRequest<Challenge> = Challenge.fetchRequest()) {
+        do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+        
+    }
+
+    
+    
+    
+}
+
+
+//MARK: - Search bar Method
+extension ChallengeYourselfViewController: UISearchBarDelegate
+{
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Challenge> = Challenge.fetchRequest()
+        request.predicate = NSPredicate(format: "nameOfTheChallenge CONTAINS[cd] %@", searchBar.text!)
+        
+        //request.sortDescriptors = [NSSortDescriptor(key: "nameOfTheChallenge", ascending: true)]
+        
+        loadItems(with: request)
+        tableView.reloadData()
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchBar.text?.count==0{
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            loadItems()
+            tableView.reloadData()
+        }
+    }
+    
+}
+
